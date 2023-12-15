aa692a11549ec549756fbcea0e5899ff340ebdf3
jundong.xjd <jundong.xjd@antfin.com>
2023-12-15 11:59:25 +0800

    改造成一个doc viewer，在 idb 同一目录下创建一个文件夹，里面放上 markdown 文件， 用户选择任意名字，可以是函数名或者注释，打开对应的 markdown 文件

--- ida_plugin/docviewida.py
+++ ida_plugin/docviewida.py
@@ -1,7 +1,7 @@
 """
-Author: Alexander Hanel
+Author: Alexander Hanel, dddong 
 Version: 1.1
-Purpose: Microsoft Document (sdk-api & Driver) document viewer for IDA. 
+Purpose: Document viewer for IDA. 
 Updates:
     * Version 1.0   - Release
     * Version 1.1   - Fixed issues with opening and closing widget 
@@ -11,29 +11,31 @@ import os
 import ida_kernwin
 import ida_idaapi
 import ida_name
+import idc 
+import idaapi 
 
 from idaapi import PluginForm
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QFont 
 
 # Path to the Markdown docs. Folder should start with 
-API_MD = r"!!CHANGE ME!!"
+IDB_DIR = os.path.dirname(idc.get_idb_path())
+API_MD = os.path.join(IDB_DIR, "Notes-" + idaapi.get_root_filename())
+if not os.path.exists(API_MD):
+    os.mkdir(API_MD)
 
 # global variables used to track initialization/creation of the forms.  
 started = False
 frm = None 
 
-def get_selected_api_name():
-    """
-    get selected item and extract function name from it 
-    via https://github.com/idapython/src/blob/e1c108a7df4b5d80d14d8b0c14ae73b924bff6f4/Scripts/msdnapihelp.py#L48
-    return: api name as string 
-    """
+def get_selected_name():
     try:
         v = ida_kernwin.get_current_viewer()
-        name, ok = ida_kernwin.get_highlight(v)
-        if not ok:
-            # print("No identifier was highlighted")
+        ret = ida_kernwin.get_highlight(v)
+        if ret is None:
+            print("No identifier was highlighted")
             return None 
+        name, flag = ret 
         t = ida_name.FUNC_IMPORT_PREFIX
         if name.startswith(t):
             return name[len(t):]
@@ -41,17 +43,25 @@ def get_selected_api_name():
     except:
         return None 
 
-class MSDN(PluginForm):
+class DocViewer(PluginForm):
     def OnCreate(self, form):
         """
         defines widget layout 
         """
+        self.api_name = None 
+        self.md_path = None 
+
         self.parent = self.FormToPyQtWidget(form)
         self.main_layout = QtWidgets.QVBoxLayout()
         self.markdown_viewer_label = QtWidgets.QLabel()
         self.markdown_viewer_label.setText("API MSDN Docs")
+        font = QFont("Microsoft YaHei")
+        font.setBold(True)
+        self.markdown_viewer_label.setFont(font)
+        
         self.markdown_viewer = QtWidgets.QTextEdit()
-        self.markdown_viewer.setReadOnly(True)
+        self.markdown_viewer.setFontFamily("Courier")
+        self.main_layout.addWidget(self.markdown_viewer_label)
         self.main_layout.addWidget(self.markdown_viewer)
         self.parent.setLayout(self.main_layout)
         self.load_markdown()
@@ -60,34 +70,56 @@ class MSDN(PluginForm):
         """
         gets api and load corresponding (if present) api markdown 
         """
-        api_name = get_selected_api_name()
-        if not api_name:
+        self.save()
+
+        self.api_name = get_selected_name()
+        if not self.api_name:
             api_markdown ="#### Invalid Address Selected"
             self.markdown_viewer.setMarkdown(api_markdown)
             return
-        md_path = os.path.join(API_MD, api_name + ".md" )
-        if os.path.isfile(md_path):
-            with open(md_path, "r") as infile:
+        self.markdown_viewer_label.setText(f"`{self.api_name}` 文档")
+
+        self.md_path = os.path.join(API_MD, self.api_name + ".md" )
+        if os.path.isfile(self.md_path):
+            with open(self.md_path, "r", encoding="utf-8") as infile:
                 api_markdown = infile.read()
         else:
-                api_markdown ="#### docs for %s could not be found" % api_name
-        self.markdown_viewer.setMarkdown(api_markdown)
+            btn_sel = idaapi.ask_yn(idaapi.ASKBTN_NO, f"{self.api_name}.md 没有找到，是否创建新的文件?")
+            if btn_sel == idaapi.ASKBTN_CANCEL or btn_sel == idaapi.ASKBTN_NO:
+                api_markdown = "!!!File not found!!!" 
+            else:
+                with open(self.md_path, "w", encoding="utf-8") as file:
+                    pass 
+                api_markdown = "" 
+            
+        self.markdown_viewer.setText(api_markdown)
 
+    def save(self):
+        # Save the content of the QTextEdit back to the Markdown file
+        # print(f"Sava back markdown content to {self.md_path}")
+        if self.api_name and os.path.isfile(self.md_path):
+            with open(self.md_path, "w", encoding="utf-8") as outfile:
+                outfile.write(self.markdown_viewer.toPlainText())
+
+            
     def OnClose(self, form):
         """
         Called when the widget is closed
         """
         global frm
         global started
+
+        self.save()
+
         del frm 
         started = False
 
-class MSDNPlugin(ida_idaapi.plugin_t):
+class DocViewerPlugin(ida_idaapi.plugin_t):
     flags = ida_idaapi.PLUGIN_MOD
-    comment = "API MSDN Docs"
+    comment = "Docs Viewer, substitude for IDA's notepad window"
     help = ""
-    wanted_name = "API MSDN Docs"
-    wanted_hotkey = "Ctrl-Shift-Z"
+    wanted_name = "Docs Viewer"
+    wanted_hotkey = "Meta-Shift-]"
 
     def init(self):
         self.options = (ida_kernwin.PluginForm.WOPN_MENU |
@@ -104,8 +136,8 @@ class MSDNPlugin(ida_idaapi.plugin_t):
             #API_MD
             if not os.path.isdir(API_MD):
                 print("ERROR: API_MD directory could not be found. Make sure to execute python run_me_first.py ")
-            frm = MSDN()
-            frm.Show("MSDN API Docs: hotkey: Ctrl-Shift-Z", options=self.options)
+            frm = DocViewer()
+            frm.Show("Docs Viewer", options=self.options)
             started = True
         else:
             frm.load_markdown()
@@ -115,4 +147,4 @@ class MSDNPlugin(ida_idaapi.plugin_t):
 
 # -----------------------------------------------------------------------
 def PLUGIN_ENTRY():
-    return MSDNPlugin()
+    return DocViewerPlugin()
